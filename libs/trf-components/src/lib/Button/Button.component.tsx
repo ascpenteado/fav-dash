a63334d71e0c141a8dc960b665b0087d6d5a2d73
Arthur Penteado <ascpenteado@gmail.com>
2023-11-15 12:21:18 -0300

    refactor: update button component and stories

--- libs/trf-components/src/lib/Button/Button.component.tsx
+++ libs/trf-components/src/lib/Button/Button.component.tsx
@@ -1,6 +1,32 @@
-import s from './Button.style.module.scss';
-const Button = () => {
-  return <button className={s.primary}>Test me</button>;
+import { FC, ReactNode, MouseEvent } from 'react';
+// import s from './Button.style.module.scss';
+
+interface ButtonProps {
+  onClick: (event: MouseEvent<HTMLButtonElement>) => void;
+  children: ReactNode;
+  disabled?: boolean;
+}
+
+const Button: FC<ButtonProps> = ({ children, onClick, disabled }) => {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      style={{
+        padding: '10px',
+        fontSize: '16px',
+        cursor: disabled ? 'not-allowed' : 'pointer',
+        backgroundColor: disabled ? '#ccc' : '#007bff',
+        color: '#fff',
+        border: 'none',
+        borderRadius: '4px',
+      }}
+      tabIndex={disabled ? -1 : 0} // Ensure the button is not focusable when disabled
+      aria-disabled={disabled} // Indicate the button's state for screen readers
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
