6dd291cd7b458c6ff47bd31d17390ac89edbb806
Arthur Penteado <ascpenteado@gmail.com>
2023-11-19 16:54:10 -0300

    feat: add modal component

---
+++ libs/trf-components/src/lib/Modal/Modal.stories.tsx
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ModalComponent from './Modal.component';
+import Button from '../Button/Button.component';
+import { ThemeColors } from '../../types/Colors';
+import { useState } from 'react';
+
+const meta: Meta<typeof ModalComponent> = {
+  component: ModalComponent,
+  argTypes: {
+    onClose: { action: 'onClose', table: { disable: true } },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof ModalComponent>;
+
+const Template = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <ModalComponent isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem sequi
+          asperiores accusamus quod ipsam omnis. Iusto aliquam totam vero ex,
+          optio sint tempora laboriosam, laudantium quisquam officia animi quia
+          libero?
+        </p>
+      </ModalComponent>
+      <Button
+        variant={ThemeColors.PrimaryColor}
+        onClick={() => setIsOpen(true)}
+      >
+        Open modal
+      </Button>
+    </>
+  );
+};
+
+export const Modal: Story = {
+  render: () => <Template></Template>,
+};
